db9942fbb2c863908211dff4c573f84551a1b292
Micheal Harker <micheal.harker@gmail.com>
2014-08-18 13:11:48 +0100

    fix errors and change tabs to spaces

--- test/plugins-new/nick.js
+++ test/plugins-new/nick.js
@@ -2,81 +2,81 @@ var coffea = require('../..');
 var Stream = require('stream').PassThrough;
 
 describe('nick.js', function() {
-	describe('on NICK', function() {
-		it('should emit "nick" [single-network]', function (done) {
-			var st1 = new Stream();
-			var client = coffea(st1);
+    describe('on NICK', function() {
+        it('should emit "nick" [single-network]', function (done) {
+            var st1 = new Stream();
+            var client = coffea(st1);
 
-			client.on("nick", function (event) {
-				event.user.getNick().should.equal('evilop');
+            client.on("nick", function (event) {
+                event.user.getNick().should.equal('evilop');
                 event.oldNick.should.equal('troll');
-				done();
-			});
+                done();
+            });
 
-			st2.write(':troll!evilop@yo.um.ad NICK evilop\r\n');
-		});
+            st1.write(':troll!evilop@yo.um.ad NICK evilop\r\n');
+        });
 
-		it('should emit "nick" [multi-network]', function (done) {
-			var st1 = new Stream();
-			var st2 = new Stream();
-			var client = coffea(st1);
-			client.useStream(st2);
+        it('should emit "nick" [multi-network]', function (done) {
+            var st1 = new Stream();
+            var st2 = new Stream();
+            var client = coffea(st1);
+            client.useStream(st2);
 
-			client.on("nick", function (event) {
-				if (event.network === 0) {
-					event.user.getNick().should.equal('ChanServ');
-                	event.oldNick.should.equal('NickServ');
-				} else {
-					event.user.getNick().should.equal('evilop');
-                	event.oldNick.should.equal('troll');
-				}
-			});
+            client.on("nick", function (event) {
+                if (event.network === 0) {
+                    event.user.getNick().should.equal('ChanServ');
+                    event.oldNick.should.equal('NickServ');
+                } else {
+                    event.user.getNick().should.equal('evilop');
+                    event.oldNick.should.equal('troll');
+                }
+            });
 
-			st1.write(':NickServ!NickServ@services. NICK ChanServ\r\n');
-			st2.write(':troll!evilop@yo.um.ad NICK evilop\r\n');
-		
-			done();
-		});
+            st1.write(':NickServ!NickServ@services. NICK ChanServ\r\n');
+            st2.write(':troll!evilop@yo.um.ad NICK evilop\r\n');
+        
+            done();
+        });
 
-		it('should emit "{network}:nick" [multi-network]', function (done) {
-			var st1 = new Stream();
-			var st2 = new Stream();
-			var client = coffea(st1);
-			client.useStream(st2);
+        it('should emit "{network}:nick" [multi-network]', function (done) {
+            var st1 = new Stream();
+            var st2 = new Stream();
+            var client = coffea(st1);
+            client.useStream(st2);
 
-			client.on("0:nick", function (event) {
-				event.user.getNick().should.equal('ChanServ');
+            client.on("0:nick", function (event) {
+                event.user.getNick().should.equal('ChanServ');
                 event.oldNick.should.equal('NickServ');
-			});
+            });
 
-			client.on("1:nick", function (event) {
-				event.user.getNick().should.equal('evilop');
+            client.on("1:nick", function (event) {
+                event.user.getNick().should.equal('evilop');
                 event.oldNick.should.equal('troll');
-			});
+            });
 
-			st1.write(':NickServ!NickServ@services. NICK ChanServ\r\n');
-			st2.write(':troll!evilop@yo.um.ad NICK evilop\r\n');			st2.write(':troll!evilop@yo.um.ad NICK evilop\r\n');
-		
-			done();
-		});
-	});
+            st1.write(':NickServ!NickServ@services. NICK ChanServ\r\n');
+            st2.write(':troll!evilop@yo.um.ad NICK evilop\r\n');            st2.write(':troll!evilop@yo.um.ad NICK evilop\r\n');
+        
+            done();
+        });
+    });
 
-	describe("on ERR_NICKNAMEINUSE", function() {
-		it("should add _ to nickname", function(done) {
-			var st1 = new Stream();
-			var client = coffea(st1);
+    describe("on ERR_NICKNAMEINUSE", function() {
+        it("should add _ to nickname", function(done) {
+            var st1 = new Stream();
+            var client = coffea(st1);
 
-			client.nick('bar');
-			st1.write(':irc.local 433 * foo :Nickname is already in use.\r\n');
+            client.nick('bar');
+            st1.write(':irc.local 433 * foo :Nickname is already in use.\r\n');
 
-			process.nextTick(function () {
-				client.getUser.getNick.should.equal('foo_');
-				st1.write(':irc.local 433 * foo_ :Nickname is already in use.\r\n');
-				process.nextTick(function () {
-					client.getUser.getNick.should.equal('foo__');
-					done();
-				});
-			});
-		});
-	});
+            process.nextTick(function () {
+                client.getUser.getNick.should.equal('foo_');
+                st1.write(':irc.local 433 * foo_ :Nickname is already in use.\r\n');
+                process.nextTick(function () {
+                    client.getUser.getNick.should.equal('foo__');
+                    done();
+                });
+            });
+        });
+    });
 });
