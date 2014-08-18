7572cacae45240097ca634492f8a226487337c89
Micheal Harker <micheal.harker@gmail.com>
2014-08-18 13:29:51 +0100

    fixed part.js test

--- test/plugins-new/part.js
+++ test/plugins-new/part.js
@@ -2,69 +2,73 @@ var coffea = require('../..');
 var Stream = require('stream').PassThrough;
 
 describe('part.js', function() {
-	describe('on PART', function() {
-		it('should emit "part" [single-network]', function (done) {
-			var st1 = new Stream();
-			var client = coffea(st1);
+    describe('on PART', function() {
+        it('should emit "part" [single-network]', function (done) {
+            var st1 = new Stream();
+            var client = coffea(st1);
+            client.nick('foo');
 
-			client.on("part", function (event) {
-				event.user.getNick().should.equal('foo');
+            client.on("part", function (event) {
+                event.user.getNick().should.equal('foo');
                 event.channels[0].getName().should.equal('#baz');
                 event.channels[1].getName().should.equal('#bar');
                 event.message.should.equal('Part');
-				done();
-			});
+                done();
+            });
 
-			st1.write(':foo!bar@baz.com PART #baz,#bar :Bye\r\n');
-		});
+            st1.write(':foo!bar@baz.com PART #baz,#bar :Part\r\n');
+        });
 
-		it('should emit "part" [multi-network]', function (done) {
-			var st1 = new Stream();
-			var st2 = new Stream();
-			var client = coffea(st1);
-			client.useStream(st2);
+        it('should emit "part" [multi-network]', function (done) {
+            var st1 = new Stream();
+            var st2 = new Stream();
+            var client = coffea(st1);
+            client.useStream(st2);
+            client.nick('foo');
 
-			client.on("part", function (event) {
-				if (event.network === 0) {
-					event.user.getNick().should.equal('ChanServ');
-                	event.channels[0].getName().should.equal('#services');
-                	event.message.should.equal(undefined);
-				} else {
-					event.user.getNick().should.equal('foo');
-                	event.channels[0].getName().should.equal('#baz');
-                	event.channels[1].getName().should.equal('#bar');
-                	event.message.should.equal('Part');
-				}
-				done();
-			});
+            client.on("part", function (event) {
+                if (event.network == 0) {
+                    event.user.getNick().should.equal('ChanServ');
+                    event.channels[0].getName().should.equal('#services');
+                    event.message.should.equal('');
+                } else {
+                    event.user.getNick().should.equal('foo');
+                    event.channels[0].getName().should.equal('#baz');
+                    event.channels[1].getName().should.equal('#bar');
+                    event.message.should.equal('Part');
+                }
+            });
 
-			st1.write(':ChanServ!ChanServ@services.in PART #services\r\n');
-			st2.write(':foo!bar@baz.com PART #baz,#bar :Bye\r\n');
-		});
+            st1.write(':ChanServ!ChanServ@services.in PART #services\r\n');
+            st2.write(':foo!bar@baz.com PART #baz,#bar :Part\r\n');
 
-		it('should emit "{network}:part" [multi-network]', function (done) {
-			var st1 = new Stream();
-			var st2 = new Stream();
-			var client = coffea(st1);
-			client.useStream(st2);
+            done();
+        });
 
-			client.on("0:part", function (event) {
-				event.user.getNick().should.equal('ChanServ');
+        it('should emit "{network}:part" [multi-network]', function (done) {
+            var st1 = new Stream();
+            var st2 = new Stream();
+            var client = coffea(st1);
+            client.useStream(st2);
+            client.nick('foo');
+
+            client.on("0:part", function (event) {
+                event.user.getNick().should.equal('ChanServ');
                 event.channels[0].getName().should.equal('#services');
-                event.message.should.equal(undefined);
-				done();
-			});
+                event.message.should.equal('');
+            });
 
-			client.on("1:part", function (event) {
-				event.user.getNick().should.equal('foo');
+            client.on("1:part", function (event) {
+                event.user.getNick().should.equal('foo');
                 event.channels[0].getName().should.equal('#baz');
                 event.channels[1].getName().should.equal('#bar');
                 event.message.should.equal('Part');
-				done();
-			});
+            });
 
-			st1.write(':ChanServ!ChanServ@services.in PART #services\r\n');
-			st2.write(':foo!bar@baz.com PART #baz,#bar :Bye\r\n');
-		});
-	});
+            st1.write(':ChanServ!ChanServ@services.in PART #services\r\n');
+            st2.write(':foo!bar@baz.com PART #baz,#bar :Part\r\n');
+
+            done();
+        });
+    });
 });
