2fe856d2b911997ea147b2565564667899d35e4c
Micheal Harker <micheal.harker@gmail.com>
2014-08-18 14:45:13 +0100

    [join.js] fixed test

--- test/plugins-new/join.js
+++ test/plugins-new/join.js
@@ -4,26 +4,31 @@ var Stream = require('stream').PassThrough;
 describe('join.js', function() {
 	describe('on JOIN', function() {
 		it('should emit "join" [single-network]', function (done) {
-			var st1 = new Stream();
-			var client = coffea(st1);
+			var client = coffea();
+            var st1 = new Stream();
+            var st1_id = client.add(st1);
+            client.nick('foo', st1_id);
 
-			client.on("join", function (event) {
+			client.once("join", function (event) {
 				event.user.getNick().should.equal('foo');
 				event.channel.should.equal('#baz');
 				done();
 			});
 
-			st1.write(':foo!bar@baz.com JOIN :#baz\r\n');
+			st1.write(':foo!bar@baz.com JOIN #baz\r\n');
 		});
 
 		it('should emit "join" [multi-network]', function (done) {
-			var st1 = new Stream();
-			var st2 = new Stream();
-			var client = coffea(st1);
-			client.useStream(st2);
+			var client = coffea();
+            var st1 = new Stream();
+            var st2 = new Stream();
+            var st1_id = client.add(st1);
+            var st2_id = client.add(st2);
+            client.nick('ChanServ', st1_id);
+            client.nick('foo', st2_id);
 
-			client.on("join", function (event) {
-				if (event.network === 0) {
+			client.once("join", function (event) {
+				if (event.network === st1_id) {
 					event.user.getNick().should.equal('ChanServ');
 					event.channel.should.equal('#servies');
 				} else {
@@ -33,30 +38,40 @@ describe('join.js', function() {
 				done();
 			});
 
-			st1.write(':ChanServ!ChanServ@services.in JOIN :#services\r\n');
-			st1.write(':foo!bar@baz.com JOIN :#baz\r\n');
+			st1.write(':ChanServ!ChanServ@services.in JOIN #services\r\n');
+			st1.write(':foo!bar@baz.com JOIN #baz\r\n');
 		});
 
 		it('should emit "{network}:join" [multi-network]', function (done) {
-			var st1 = new Stream();
-			var st2 = new Stream();
-			var client = coffea(st1);
-			client.useStream(st2);
+			var client = coffea();
+            var st1 = new Stream();
+            var st2 = new Stream();
+            var st1_id = client.add(st1);
+            var st2_id = client.add(st2);
+            client.nick('ChanServ', st1_id);
+            client.nick('foo', st2_id);
 
-			client.on("0:join", function (event) {
+            var tests = 0;
+			client.on(st1_id + ":join", function (event) {
 				event.user.getNick().should.equal('ChanServ');
 				event.channel.should.equal('#servies');
-				done();
+				tests++;
+                if(tests >= 2) {
+                    done();
+                }
 			});
 
-			client.on("1:join", function (event) {
+			client.on(st2_id + ":join", function (event) {
 				event.user.getNick().should.equal('foo');
 				event.channel.should.equal('#baz');
-				done();
+				tests++;
+                if(tests >= 2) {
+                    done();
+                }
 			});
 
-			st1.write(':ChanServ!ChanServ@services.in JOIN :#services\r\n');
-			st1.write(':foo!bar@baz.com JOIN :#baz\r\n');
+			st1.write(':ChanServ!ChanServ@services.in JOIN #services\r\n');
+			st1.write(':foo!bar@baz.com JOIN #baz\r\n');
 		});
 	});
 });
