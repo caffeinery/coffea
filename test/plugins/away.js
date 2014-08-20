0cd2812d8c601e82350a332cfe19e4c511e4f5ce
Micheal Harker <micheal.harker@gmail.com>
2014-08-20 22:02:08 +0100

    test/away: make test faster

--- test/plugins/away.js
+++ test/plugins/away.js
@@ -10,12 +10,14 @@ describe('away.js', function () {
         it('should emit "away"', function (done) {
             var stream = new Stream(),
                 client = irc(stream, false);
+            client.nick('mynick');
 
             client.on('away', function (event) {
                 event.user.getNick().should.equal('targetnick');
                 event.message.should.equal('nope');
                 done();
             });
+
             stream.write(':irc.local 301 mynick targetnick :nope\r\n');
         });
     });
