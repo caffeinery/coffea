a4e2f4da54e67d447db4b9fcb437632c9f8137c9
Micheal Harker <micheal.harker@gmail.com>
2014-08-22 12:31:18 +0100

    new-test/cap-(account|away)-join: added test

---
+++ test/plugins-new/cap/cap-away-notify.js
@@ -0,0 +1,23 @@
+var coffea = require('../../..');
+var Stream = require('stream').PassThrough;
+var should = require('should');
+
+describe('cap-away-notify.js', function() {
+    describe('on AWAY', function() {
+        it('should emit "away-notify"', function (done) {
+            var client = coffea();
+            var st1 = new Stream();
+            var st1_id = client.add(st1);
+            client.nick('foo', st1_id);
+
+            client.once('away-notify', function (event) {
+                event.user.getNick().should.equal('mike');
+                event.message.should.equal('not here');
+                done();
+            });
+
+            st1.write(':irc.server CAP * ACK :away-notify\r\n');
+            st1.write(':mike!mike@wants.a.vhost.so.badly.he.would.suck.brendens.dick AWAY :not here\r\n');
+        });
+    });
+});
