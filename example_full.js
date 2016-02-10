616e0bcab1da3ed4f8a4a66b1daa85924b52b53a
Daniel Bugl <me@omnidan.net>
2016-02-10 21:48:51 +0100

    add full example

---
+++ example_full.js
@@ -0,0 +1,58 @@
+import connect, { message, mapEvents } from './src/index'
+
+// protocol
+const messageHandler = dispatch =>
+  e => console.log('message received:', e.text)
+
+const defaultHandler = dispatch =>
+  e => dispatch({
+    type: 'error',
+    text: 'Unknown event'
+  })
+
+const connectTo = (token, dispatch) =>
+  setTimeout(() => {
+    dispatch({
+      type: 'connect',
+      token: token
+    })
+  }, 100)
+
+const registerEvents = dispatch =>
+  setInterval(() => {
+    dispatch({
+      type: 'message',
+      channel: '#test',
+      text: Math.random().toString(36).substring(7)
+    })
+  }, 250)
+
+const exampleProtocol = (config, dispatch) => {
+  connectTo(config.token, dispatch)
+  registerEvents(dispatch)
+  return mapEvents({
+    'message': messageHandler,
+    'default': defaultHandler
+  })
+}
+// /protocol
+
+// app
+const networks = connect([
+  {
+     protocol: exampleProtocol,
+     token: '...'
+  }
+])
+
+networks.on('event', e => console.log(e))
+
+const reverse = (msg, send) => {
+  const reversedText = msg.text.split('').reverse().join('')
+  const reversedMessage = message(msg.channel, reversedText)
+
+  send(reversedMessage)
+}
+
+networks.on('message', reverse)
+// /app
