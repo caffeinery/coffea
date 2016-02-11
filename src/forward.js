8ef1761b622830323da6c5fc71e04bb1d749f25a
Daniel Bugl <me@omnidan.net>
2016-02-11 23:49:45 +0100

    forward: better debug output

--- src/forward.js
+++ src/forward.js
@@ -1,5 +1,5 @@
 import { makeLogger } from './utils'
-const { info, warn } = makeLogger('forward')
+const { debug, info, warn } = makeLogger('forward')
 
 /**
  * Higher-order function that returns a function, which forwards events based on
@@ -14,7 +14,8 @@ export default function forward (mapping) {
     const { type } = event
     if (mapping.hasOwnProperty(type)) {
       if (typeof mapping[type] === 'function') {
-        info(`forwarded "${type}" event to function "${mapping[type].name}"`)
+        info(`forwarded "${type}" event to function`)
+        debug(`function: ${mapping[type].toString()}`)
         return mapping[type](event)
       } else warn(`invalid mapping for "${type}" event (not a function!)`)
     } else warn(`"${type}" event not mapped`)
